a091aeec5e04936e0513ffc9705af5dd9ef6c196
KOSHIKAWA Kenichi <reishoku.misc@pm.me>
2022-04-14 21:50:11 +0900

    explicitly specify default branch
    Signed-off-by: KOSHIKAWA Kenichi <reishoku.misc@pm.me>

--- goresponsiveness.rb
+++ goresponsiveness.rb
@@ -2,7 +2,7 @@ class Goresponsiveness < Formula
   desc "A draft-ietf-ippm-responsiveness client in Go."
   homepage "https://github.com/network-quality/goresponsiveness"
 
-  head "https://github.com/network-quality/goresponsiveness.git"
+  head "https://github.com/network-quality/goresponsiveness.git", branch: "main"
 
   depends_on "go" => :build
 
