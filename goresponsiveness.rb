d574b10b54eb86f619e4dece87832279e27bbb2c
KOSHIKAWA Kenichi <reishoku.misc@pm.me>
2022-04-14 21:47:26 +0900

    add goresponsiveness.rb
    Signed-off-by: KOSHIKAWA Kenichi <reishoku.misc@pm.me>

---
+++ goresponsiveness.rb
@@ -0,0 +1,14 @@
+class GoResponsiveness < Formula
+  desc "A draft-ietf-ippm-responsiveness client in Go."
+  homepage "https://github.com/network-quality/goresponsiveness"
+
+  head "https://github.com/network-quality/goresponsiveness.git"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "mod", "download"
+    system "go", "build", "networkQuality.go", "-o", bin/"networkQuality"
+  end
+end
+
